3c41276aef98fcb2905a36adb63239750f9ba8e4
Paradox <paradoxlegendz@gmail.com>
2025-03-06 17:58:03 -0600

    experiment 3 draft

--- zigbee_usart/firmware/src/worker.c
+++ zigbee_usart/firmware/src/worker.c
@@ -1,51 +1,3 @@
-/*******************************************************************************
-  USB UART click routine example source file
-
-  Company
-    Microchip Technology Inc.
-
-  File Name
-    usb_uart_example.c
-
-  Summary
-    USB UART click routine example implementation file.
-
-  Description
-    This file defines the usage of the USB UART click routine APIs.
-
-  Remarks:
-    None.
-
- *******************************************************************************/
-
-// DOM-IGNORE-BEGIN
-/*
-    (c) 2021 Microchip Technology Inc. and its subsidiaries. You may use this
-    software and any derivatives exclusively with Microchip products.
-
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
-    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-
-    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
-    TERMS.
-*/
-// DOM-IGNORE-END
-
-/**
-  Section: Included Files
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
@@ -53,121 +5,84 @@
 #include "click_routines/usb_uart/usb_uart.h"
 #include <xc.h>
 #include "utils.h"
-// setup our heartbeat to be 1ms: we overflow at 1ms intervals with a 48MHz clock
-// uses the SysTicks unit so that we get reliable debugging (timer stops on breakpoints)
-// this is a countdown timer that sends an interrupt when 0
-#define MS_TICKS 48000UL
 
-// number of millisecond between LED flashes
-#define LED_FLASH_MS 1000UL
-#define PATTERN_LENGTH 14 // Length of "RAW:-xxx,hi\r\n"
-
-
-//// NOTE: this overflows every ~50 days, so I'm not going to care here...
-// volatile uint32_t msCount = 0;
-
-/**
-  Section: Example Code
- */
-// AT+SENDMCAST:01,FFFF,01,01,C091,0002,Test
-// AT+RDATAB:XX then data of size XX
-
-//
-// uint8_t restart[] = "ATZ\r";
-//
-// uint8_t join_own_network[] = "AT+EN\r";
-// uint8_t change_channel[] = "AT+CCHANGE:10\r"; // that's 10 hex! channels go from 11-26, aka 0B to 1A
-
-char out_string[200];
-
-//// Fires every 1ms
-// void SysTick_Handler()
-//{
-//   msCount++;
-// }
+// Define the AT commands
+uint8_t set_power_mode_3[] = "ATS39=0003\r"; // Set power mode to 3 (sleep)
+uint8_t wakeup_command[] = "+++\r";          // Command to wake up
+uint8_t listen_command[] = "AT+POLL\r";      // Command to listen for messages
+uint8_t check_voltage[] = "ATS3D?\r";        // Check battery voltage
 
 void worker_main(void)
 {
-
   systemInitialize();
 
   uint8_t read_chars[100];
-  uint8_t buffer[10];
+  //  uint8_t buffer[10];
 
-  usb_uart_USART_Write(restart, 4);
+  // Restart the module
+  usb_uart_USART_Write(restart, strlen((char *)restart));
   while (usb_uart_USART_WriteIsBusy())
     ;
-  // need to wait for an "OK" to come through
-  usb_uart_USART_Read((uint8_t *)&read_chars, 2);
-  while (usb_uart_USART_ReadIsBusy())
-    ; // do nothing
 
-  usb_uart_USART_Write(join_own_network, 6);
+  //  usb_uart_USART_Read((uint8_t *)&read_chars, 2);
+  //  while (usb_uart_USART_ReadIsBusy())
+  //    ;
+  memset(read_chars, '\0', sizeof(read_chars));
+  readAllBytesWithTimeout((uint8_t *)&read_chars, sizeof(read_chars));
+
+  // Join the network
+  usb_uart_USART_Write(join_own_network, strlen((char *)join_own_network));
   while (usb_uart_USART_WriteIsBusy())
     ;
-  // need to wait for an "OK" to come through
-  usb_uart_USART_Read((uint8_t *)&read_chars, 2);
-  while (usb_uart_USART_ReadIsBusy())
-    ; // do nothing
 
-  usb_uart_USART_Write(change_channel, 14);
+  memset(read_chars, '\0', sizeof(read_chars));
+  readAllBytesWithTimeout((uint8_t *)&read_chars, sizeof(read_chars));
+
+  // Change to a specific channel
+  usb_uart_USART_Write(change_channel, strlen((char *)change_channel));
   while (usb_uart_USART_WriteIsBusy())
     ;
-  // need to wait for an "OK" to come through
-  usb_uart_USART_Read((uint8_t *)&read_chars, 2);
-  while (usb_uart_USART_ReadIsBusy())
-    ; // do nothing
 
-  // preload loop
-  // start reading
-  usb_uart_USART_Read((uint8_t *)&read_chars, 1);
-  printf("Let's go\n");
+  memset(read_chars, '\0', sizeof(read_chars));
+  readAllBytesWithTimeout((uint8_t *)&read_chars, sizeof(read_chars));
 
-  uint8_t valid_command_rdatab[] = "AT+RDATAB:02\r";
-  uint8_t valid_message_rdatab[] = "hi\r";
-  int i = 50;
-  while (i > 0)
-  {
-    if ((getMsCount() % 350UL) == 0)
-    {
-      usb_uart_USART_Write(valid_command_rdatab, strlen((char *)valid_command_rdatab));
-      while (usb_uart_USART_WriteIsBusy())
-        ;
+  printf("Let's go\r\n");
 
-      usb_uart_USART_Write(valid_message_rdatab, strlen((char *)valid_message_rdatab));
-      while (usb_uart_USART_WriteIsBusy())
-        ;
-
-      printf("Sent message %d\r\n", i--);
-    }
-  }
- 
+  // Main loop: Wake, listen, sleep
   while (1)
   {
-    // wait for an interrupt
-    __WFI();
+    // Wake up the device
+    usb_uart_USART_Write(wakeup_command, strlen((char *)wakeup_command));
+    while (usb_uart_USART_WriteIsBusy())
+      ;
+    memset(read_chars, '\0', sizeof(read_chars));
+    readAllBytesWithTimeout((uint8_t *)&read_chars, sizeof(read_chars));
 
-    if (!usb_uart_USART_ReadIsBusy())
+    printf("Awake\r\n");
+
+    // Check battery voltage
+    usb_uart_USART_Write(check_voltage, strlen((char *)check_voltage));
+    while (usb_uart_USART_WriteIsBusy())
+      ;
+    printf("Checking voltage\r\n");
+
+    memset(read_chars, '\0', sizeof(read_chars));
+    readAllBytesWithTimeout((uint8_t *)&read_chars, sizeof(read_chars));
+
+    printf("Battery Voltage: %s\r\n", read_chars);
+
+    // Set power mode back to sleep (mode 3)
+    usb_uart_USART_Write(set_power_mode_3, strlen((char *)set_power_mode_3));
+    while (usb_uart_USART_WriteIsBusy())
+      ;
+    memset(read_chars, '\0', sizeof(read_chars));
+    readAllBytesWithTimeout((uint8_t *)&read_chars, sizeof(read_chars));
+
+    // Sleep for 1 minute (60000 milliseconds)
+    for (int i = 0; i < 60; i++)
     {
-      sprintf(out_string, "%c", read_chars[0]);
-      // usb_uart_USART_Write((uint8_t *)out_string,sizeof(out_string));
-      printf(out_string);
-
-      // read the next one
-      usb_uart_USART_Read((uint8_t *)&read_chars, 1);
+      __WFI();          // Wait for interrupt, power-saving
+      handleLEDBlink(); // Maintain LED state
     }
-    SERCOM5_USART_Read(&buffer, 10, true);
-    if (buffer[0] != '\0')
-    {
-      // printf((char*)&buffer); // comment this out if your serial program does a local echo.
-      //  send the string to the zigbee one char at a time
-      for (int i = 0; i < 10 && buffer[i] != '\0'; i++)
-      {
-        usb_uart_USART_Write((uint8_t *)&buffer[i], 1);
-      }
-    }
-
-    // handle blinking the light
-    handleLEDBlink();
   }
 }
